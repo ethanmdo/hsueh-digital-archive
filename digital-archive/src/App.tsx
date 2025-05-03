6d745d07fa3a68b0cafe45480bed810897d93f73
Tabitha <tabitha_lynn@brown.edu>
2025-05-03 11:37:54 -0400

    Changed home path

--- digital-archive/src/App.tsx
+++ digital-archive/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home/>}/>
+          <Route path='/hsueh-digital-archive' element={<Home/>}/>
           <Route path="/Photographs" element={<Photographs/>}/>
         </Routes>
       </BrowserRouter>
