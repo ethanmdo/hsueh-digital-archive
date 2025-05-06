701c04cbbf2cbe6279c1ff05af267eb44fdb6a4c
Ethan Do <ethando@gmail.com>
2025-05-05 20:46:56 -0400

    home route

--- digital-archive/src/App.tsx
+++ digital-archive/src/App.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import './App.css';
-import Home from './pages/home/home';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Photographs from './pages/Photographs/Photographs';
-import ThemedCollections from './pages/themedCollections/themedCollections';
-import TaiwanMilitary from './pages/taiwanMilitary/taiwanMilitary';
-
+import React from "react";
+import "./App.css";
+import Home from "./pages/home/home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Photographs from "./pages/Photographs/Photographs";
+import ThemedCollections from "./pages/themedCollections/themedCollections";
+import TaiwanMilitary from "./pages/taiwanMilitary/taiwanMilitary";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/hsueh-digital-archive" element={<Home/>}/>
-          <Route path="/photographs" element={<Photographs/>}/>
-          <Route path="/themed-collections" element={<ThemedCollections/>}/>
-          <Route path="/taiwan-military" element={<TaiwanMilitary/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/hsueh-digital-archive" element={<Home />} />
+          <Route path="/photographs" element={<Photographs />} />
+          <Route path="/themed-collections" element={<ThemedCollections />} />
+          <Route path="/taiwan-military" element={<TaiwanMilitary />} />
         </Routes>
       </BrowserRouter>
     </div>
